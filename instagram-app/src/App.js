bcc3db6816244b94bd162720fc81570c2cdb0290
jrl508 <jrl47@yahoo.com>
2019-06-05 17:37:40 -0400

    built out search bar functionality

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -8,8 +8,19 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      posts:[]
+      posts:[],
+      filteredPosts:[]
     };
+  }
+
+  changeHandler = e =>{
+    console.log(e.target.value)
+    e.preventDefault();
+    this.setState(
+        {
+            [e.target.name]: e.target.value
+        }
+    )
   }
 
   componentDidMount(){
@@ -19,11 +30,34 @@ class App extends Component {
     });
   }
 
+  searchFilter=e=>{
+    const filtered = this.state.posts.filter(post => 
+      post.username.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+      this.setState({filteredPosts: filtered})
+  }
+
   render(){
   return (
     <div className="App">
-    <SearchBar/>
-    <PostContainer posts={this.state.posts} changeHandler={this.changeHandler}/>
+        <div className="nav">
+        <ul className="nav-bar">
+          <li> <a href='#' className="logo"><i className="fab fa-instagram icon"></i> | Instagram </a></li>
+          <SearchBar 
+          changeHandler={this.changeHandler} 
+          searchFilter={this.searchFilter}
+          />
+          <li><a href="#"><i className="far fa-compass icon"></i></a></li>
+          <li><a href="#"><i className="far fa-heart icon"></i></a></li>
+          <li><a href="#"><i className="far fa-user icon"></i></a></li>
+        </ul>
+    </div>
+
+    <PostContainer
+      filteredPosts={this.state.filteredPosts} 
+      posts={this.state.posts} 
+      changeHandler={this.changeHandler}
+    />
     </div>
   );
   }
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './PostContainer.css'
 import CommentSection from '../CommentSection/CommentSection';
-function PostContainer(props){
-    return(
-        <div className="post-container">
-            {props.posts.map(post=>(
-                <div key={post.id} className="post">
-                    <div className="post-header"><img src={post.thumbnailUrl} alt="thumbnail" className="thumbnail"></img><strong>{post.username}</strong></div>
-                    <img src={post.imageUrl} alt="post-img" className="post-img"></img>
-                
-                    <CommentSection comments={post.comments} timestamp={post.timestamp} likes={post.likes}/>
-                </div>
-
-            ))}
-        </div>
-    )
+class PostContainer extends Component{
+    render(){
+        return(
+            <div className="post-container">
+                {this.props.filteredPosts.length === 0 ? this.props.posts.map(post=>{
+                    return(
+                        <div key={post.id} className="post">
+                            <div className="post-header">
+                                <img src={post.thumbnailUrl}
+                                alt="thumbnail"
+                                className="thumbnail">
+                                </img>
+                                <strong>{post.username}</strong>
+                            </div>
+                            <img src={post.imageUrl} alt="post-img" className="post-img"></img>
+                        
+                            <CommentSection 
+                                comments={post.comments} 
+                                timestamp={post.timestamp} 
+                                likes={post.likes}
+                            />
+                        </div>
+                    )
+                }) : this.props.filteredPosts.map(post=>{
+                return(
+                    <div key={post.id} className="post">
+                        <div className="post-header">
+                            <img src={post.thumbnailUrl}
+                            alt="thumbnail"
+                            className="thumbnail">
+                            </img>
+                            <strong>{post.username}</strong>
+                        </div>
+                        <img src={post.imageUrl} alt="post-img" className="post-img"></img>
+                    
+                        <CommentSection 
+                            comments={post.comments} 
+                            timestamp={post.timestamp} 
+                            likes={post.likes}
+                        />
+                    </div>
+                )
+                })
+        
+            }
+            </div>
+        )
+    }
 }
 
 export default PostContainer;
